65a3bcd11e55e978dd3b157c766eb4906f571915
lujiaguan <381090826@qq.com>
2018-07-04 07:05:42 +0800

    wallet

---
+++ src/components/Asset.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page-part">
+    <mt-cell class="cell"
+             title="TG-COIN"
+             to=""
+             is-link
+             v-model="balance">
+      <img slot="icon" src="../assets/logo.png" width="24" height="24">
+    </mt-cell>
+  </div>
+</template>
+<script>
+  import httpUtils from '../util/HttpUtils'
+
+  export default {
+    name: 'page-tabbar',
+    data() {
+      return {
+        balance: 0,
+      };
+    }, created: function () {
+      const _this = this;
+      httpUtils.post("/api/getBalance", "")
+        .then(function (res) {
+          console.log(res.data);
+          _this.balance = res.data;
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+    }
+  }
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,23 +7,12 @@
       <div class="body">
         <mt-tab-container class="page-tabbar-container" v-model="selected">
           <mt-tab-container-item id="资产">
-            <div class="page-part">
-              <mt-cell class="cell"
-                       title="ETH"
-                       to="//github.com"
-                       is-link
-                       value="100.00000000000">
-                <img slot="icon" src="../assets/logo.png" width="24" height="24">
-              </mt-cell>
-              <mt-cell class="cell"
-                       title="TG-COIN"
-                       to="//github.com"
-                       is-link
-                       v-model="balance"
-                       value="">
-                <img slot="icon" src="../assets/logo.png" width="24" height="24">
-              </mt-cell>
+            <asset></asset>
+            <div>
+              您的收款二维码：
+              <vue-q-art :config="config"></vue-q-art>
             </div>
+
           </mt-tab-container-item>
           <mt-tab-container-item id="转入">
 
@@ -33,15 +22,13 @@
           </mt-tab-container-item>
           <mt-tab-container-item id="我的">
             <div class="page-part">
-
               <mt-cell
                 title="实名认证"
-                to="//github.com"
+                to=""
                 is-link
                 value="未认证">
                 <img slot="icon" src="../assets/logo.png" width="24" height="24">
               </mt-cell>
-
             </div>
             <div class="sing-out">
               <router-link to="/">
@@ -75,75 +62,27 @@
   </div>
 </template>
 <script>
-  import Vue from 'vue'
-  import axios from 'axios'
-  import Qs from 'qs'
+  import asset from '../components/Asset'
+  import VueQArt from 'vue-qart'
 
   export default {
-
     name: 'page-tabbar',
-
     data() {
       return {
         selected: '资产',
-        // balance: this.post("http://localhost:9022/api/getBalance", "")
-        balance: 0
+        config: {
+          value: 'http://127.0.0.1:9022',
+          imagePath: '../assets/logo.png',
+          color: 'red',
+          filter: 'color',
+          size: '500px'
+        }
       };
+    }, components: {
+      asset,
+      VueQArt
     },
-    methods: {
-      post: function (url, data) {
-        axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-        let config = {
-          //请求的接口，在请求的时候，如axios.get(url,config);这里的url会覆盖掉config中的url
-          url: url,
-
-          // 请求方法同上
-          method: 'post', // default
-          // 基础url前缀
-          baseURL: 'http://120.79.191.116:1444',
-
-          transformRequest: [function (data) {
-            // 这里可以在发送请求之前对请求数据做处理，比如form-data格式化等，这里可以使用开头引入的Qs（这个模块在安装axios的时候就已经安装了，不需要另外安装）
-            data = Qs.stringify({});
-            return data;
-          }],
-
-          transformResponse: [function (data) {
-            // 这里提前处理返回的数据
-
-            return data;
-          }],
-
-          // 请求头信息
-          headers: {'X-Requested-With': 'XMLHttpRequest'},
-
-          //parameter参数
-          params: {
-            timestamp: Date.parse(new Date()) / 1000
-          },
-
-          //post参数，使用axios.post(url,{},config);如果没有额外的也必须要用一个空对象，否则会报错
-          data: data,
-
-          //设置超时时间
-          timeout: 5000,
-          //返回数据类型
-          responseType: 'json', // default
-        };
-        return axios.post(url, data, config)
-      }
-    }, created: function () {
-      this.post("/api/getBalance", "")
-        .then(function (res) {
-          console.log(res.data);
-          balance = res.data;
-        })
-        .catch(function (err) {
-          console.log(err)
-        })
-    }
   }
-  ;
 </script>
 
 <style>
