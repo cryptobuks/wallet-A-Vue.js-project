c47032253d6c3d62bf655bba0b597ff30a79402d
lujiaguan <381090826@qq.com>
2018-07-18 19:19:42 +0800

    注册

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,23 +6,21 @@
       </router-link>
     </mt-header>
 
-    <mt-field label="用户名" placeholder="" v-model="username" :state="state.username"
+    <mt-field label="账号" placeholder="请输入账号" v-model="input.username" :state="state.username"
               @input="checkRule('username')"></mt-field>
-    <mt-field label="邮箱" placeholder="" type="email" v-model="email" :state="state.email"
-              @input="checkRule('email')"></mt-field>
-    <mt-field label="密码" placeholder="最少6位" type="password" v-model="password" :state="state.password"
-              @input="checkRule('password')"></mt-field>
-    <mt-field label="手机号" placeholder="" type="number" v-model="phone" :state="state.phone"
+    <mt-field label="手机号" placeholder="请输入手机号" type="number" v-model="input.phone" :state="state.phone"
               @input="checkRule('phone')"></mt-field>
-    <mt-field label="生日" placeholder="" type="date" v-model="birthday" :state="state.birthday"
-              @input="checkRule('birthday')"></mt-field>
-    <mt-field label="验证码" v-model="captcha" type="number" :state="state.captcha"
+    <mt-field label="验证码" placeholder="请输入6位的验证码" v-model="input.captcha" type="number" :state="state.captcha"
               @input="checkRule('captcha')">
-      <span style="color: #b5b5b5">60秒后重新获取</span>
+      <span style="color: #b5b5b5" @click="sendCode">点击获取</span>
     </mt-field>
+    <mt-field label="密码" placeholder="请输入6位或以上的密码" type="password" v-model="input.password" :state="state.password"
+              @input="checkRule('password')"></mt-field>
+
     <div class="submit">
       <mt-button type="primary" @click="register" class="button">下一步</mt-button>
     </div>
+
   </div>
 </template>
 <style>
@@ -36,86 +34,76 @@
 </style>
 <script>
   import {Toast} from 'mint-ui';
+  import TGCoinHttpUtils from '../util/TGCoinHttpUtils'
 
   export default {
     name: 'register',
     data() {
       return {
-        username: "",
-        email: "",
-        password: "",
-        phone: "",
-        website: "",
-        number: "",
-        birthday: "",
-        introduction: "",
-        captcha: "",
+        input: {
+          username: "",
+          phone: "",
+          password: "",
+          captcha: "",
+        },
         state: {
           username: "",
-          email: "",
-          password: "",
           phone: "",
-          birthday: "",
+          password: "",
           captcha: "",
+        },
+        roules: {
+          username: /^[a-zA-Z0-9]{4,20}$/,
+          phone: /^[0-9]{11,20}$/,
+          password: /^[a-zA-Z0-9]{6,20}$/,
+          captcha: /^[0-9]{6}$/,
         }
       };
     }, methods: {
       register() {
-        Toast('注册成功');
-        localStorage.setItem('sessionKey', 'as');
-        this.$router.push('/Home');
+        let stateMap = this.state;
+        let router = this.$router;
+        let isSuccess = true;
+        Object.keys(stateMap).forEach(function (key) {
+          if (stateMap[key] === '' || stateMap[key] !== 'success') {
+            Toast('请核对信息再提交');
+            isSuccess = false;
+          }
+        });
+
+        if (isSuccess) {
+          TGCoinHttpUtils.post('/app/api/register', this.input)
+            .then(function (res) {
+              console.log(res);
+              if (res.data.code === 100) {
+                Toast('注册成功');
+                localStorage.setItem('sessionKey', 'as');
+                router.push('/Home');
+              }
+            })
+            .catch(function (err) {
+              console.log(err)
+            });
+        }
       },
       checkRule(type) {
+        let data = this.input;
+        let state = this.state;
+        let roules = this.roules;
         let ret = 'error';
-        switch (type) {
-          case 'username':
-            if (/^[a-zA-Z0-9]{4,20}$/.test(this.username)) {
-              ret = 'success'
-            }
-            if (this.username.length === 0) {
-              ret = ''
-            }
-            this.state.username = ret;
-            break;
-          case 'email':
-            if (/^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(this.email)) {
-              ret = 'success'
-            }
-            if (this.email.length === 0) {
-              ret = ''
-            }
-            this.state.email = ret;
-            break;
-          case 'password':
-            if (/^[a-zA-Z0-9]{6,20}$/.test(this.password)) {
-              ret = 'success'
-            }
-            if (this.password.length === 0) {
-              ret = ''
-            }
-            this.state.password = ret;
-            break;
-          case 'phone':
-            if (/^[0-9]{11,20}$/.test(this.phone)) {
-              ret = 'success'
-            }
-            if (this.phone.length === 0) {
-              ret = ''
-            }
-            this.state.phone = ret;
-            break;
-          case 'captcha':
-            if (/^[0-9]{6}$/.test(this.captcha)) {
-              ret = 'success'
-            }
-            if (this.captcha.length === 0) {
-              ret = ''
-            }
-            this.state.captcha = ret;
-            break;
-          default:
-            break;
+        Object.keys(roules).forEach(function (key) {
+          if (key === type && roules[key].test(data[type])) {
+            ret = 'success';
+            return true;
+          }
+        });
+        if (data[type].length === 0) {
+          ret = ''
         }
+        state[type] = ret
+      },
+      sendCode() {
+        Toast('您正在注册感恩链账号，验证码为123456');
       }
     }
   }
